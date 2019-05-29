08aa558c84ff0b2e50f4f883a6e90cd8d0c5ffc0
Sylvain Chaugny <sylvain@slaask.com>
2019-05-29 11:25:32 +0200

    add

--- modules/LesTerroristesDeThiercelieux.js
+++ modules/LesTerroristesDeThiercelieux.js
@@ -18,8 +18,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		if (this.data.commands[this.data.command.name]) {
 			this[this.data.commands[this.data.command.name]](message)
 		} else {
-			var channel = this.data.init ? this.data.channel : message.channel
-			channel.send("<@" + message.author.id + "> a encore fait de la merde, ` " + this.generateCommand(this.data.command.name) + " ` ça existe pas :rage:")
+			this.send(`${this.generateCommand(this.data.command.name)} ça existe pas :rage:`, this.data.init ? this.data.channel : message.channel)
 		}
 	}
 
@@ -29,22 +28,31 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 	commandInit(message) {
 		this.data.channel = message.guild.channels.find('name', this.config.channel_name)
-		this.data.init = true
+		if (this.data.channel.id != message.channel.id) {
+			this.send(`Faut le faire dans <#${this.data.channel.id}>, gardons ce serveur calme et serein :3`, message.channel)
+		} else {
+			this.data.init = true
+		}
 	}
 
 	commandRegister(message) {
 		if (!this.checkIfInitialized(message)) {
 			return ;
 		}
-		console.log("commandRegister")
-		this.data.channel.send("register")
+		this.registerMember(message.author, message.guild.members.get(message.author.id).nickname, this.parseCommand(message.content, false).slice(1))
 	}
 
 	commandLaunch(message) {
+		if (!this.checkIfInitialized(message)) {
+			return ;
+		}
 		console.log("commandLaunch")
 	}
 
 	commandVote(message) {
+		if (!this.checkIfInitialized(message)) {
+			return ;
+		}
 		console.log("commandVote")
 	}
 
@@ -53,12 +61,31 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 
+// REGISTER
+
+
+	registerMember(member, nickname, custom_nickname) {
+		nickname = nickname ? nickname : member.username
+		var l = this.data.members.push({id: member.id, nickname: custom_nickname != "" ? custom_nickname : nickname, original_nickname: custom_nickname != "" ? nickname : null})
+		this.send(`**${this.data.members[l - 1].nickname}** a rejoint la partie, ${["on s'enjaille", "on s'amuse", "vous pouvez quitter du coup", "niquez-le"][Utils.rand(3)]}`)
+		console.log(this.data.members)
+	}
+
+
+// MESSAGING
+
+
+	send(txt, channel = null) {
+		(channel ? channel : this.data.channel).send(txt)
+	}
+
+
 // INIT AND MISC
 
 
 	checkIfInitialized(message) {
 		if (!this.data.init) {
-			message.channel.send("Mshllh du calme voyons, fais d'abord un ` " + this.generateCommand("init") + " ` avant <@" + message.author.id + ">")
+			this.send(`Du calme voyons, faut d'abord un ${this.generateCommand("init")}`, message.channel)
 			return false;
 		}
 		return true
@@ -84,15 +111,18 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		}
 	}
 
-	parseCommand(txt) {
+	parseCommand(txt, fill_data = true) {
 		var cmd = txt.split(" ")
 		cmd.splice(0, 1)
-		this.data.command.name = cmd.splice(0, 1)[0]
-		this.data.command.args = cmd
+		if (fill_data) {
+			this.data.command.name = cmd[0]
+			this.data.command.args = cmd.slice(1)
+		}
+		return cmd
 	}
 
 	generateCommand(cmd) {
-		return this.constants.command_prefix + this.config.command_name + " " + cmd
+		return `\`${this.constants.command_prefix + this.config.command_name} ${cmd}\``
 	}
 }
 
