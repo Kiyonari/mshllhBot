cb74f393b0288466a523ed1119b6734368d74180
Sylvain Chaugny <sylvain@slaask.com>
2019-06-07 17:40:41 +0200

    add

--- modules/LesTerroristesDeThiercelieux.js
+++ modules/LesTerroristesDeThiercelieux.js
@@ -47,6 +47,396 @@ const c_play_order = ["clairvoyant", "werewolf", "witch"]
 class LesTerroristesDeThiercelieux extends BaseModule {
 	constructor(conf) {
 		conf.command_name = "graou";
+		super(conf)
+		this.initData()
+	}
+
+	canProcess(message) {
+		return message.content.split(" ")[0] == this.constants.command_prefix + this.config.command_name
+	}
+
+	process(message) {
+		this.parseCommand(message.content)
+		if (this.data.waiting_command && this.data.command.name != this.data.waiting_command) {
+			this.send(`Calmons-nous <@${message.author.id}>, j'attends quelqu'un et après on verra pour toi :3`, this.data.status.length ? null : message.channel)
+		} else {
+			var command = this.data.commands.find((i) => (i.id == this.data.command.name))
+			if (command) {
+				if (this.canTriggerCommand(message, command)) {
+					this[command.method](message)
+				}
+			} else {
+				this.send(`${this.generateCommand(this.data.command.name)} ça existe pas :rage:`, this.data.status.length ? null : message.channel)
+			}
+		}
+	}
+
+	canTriggerCommand(message, command) {
+		var current_channel = this.data.channels.config.find((i) => (i.discord_id == message.channel.id))
+		if (!command.authorized_channels.includes(current_channel.id)) {
+			var channels_list = []
+			for (var chan of command.authorized_channels) {
+				channels_list.push(this.data.channels.config.find((i) => (i.id == chan)).name)
+			}
+			this.send(`Calmons-nous <@${message.author.id}>, il faut faire ça dans un de ces channels: \`${channels_list.join(", ")}\``, message.channel)
+			return false
+		}
+		if (command.needs_init) {
+			if (command.needs_start) {
+				if (this.data.status != 'start') {
+					this.send(`Calmons-nous <@${message.author.id}>, faut d'abord faire un ${this.generateCommand("start")} pour ça !`, message.channel)
+					return false
+				}
+				return true
+			}
+			if (this.data.status != 'init') {
+				this.send(`Calmons-nous <@${message.author.id}>, faut d'abord faire un ${this.generateCommand("init")} pour ça !`, message.channel)
+				return false
+			}
+			return true
+		}
+		return true
+	}
+
+	commandTest(message) {
+	}
+
+	initChannels() {
+		for (var conf of this.data.channels.config) {
+			this.data.channels[conf.id] = this.data.guild.channels.find((found) => (found.id == conf.discord_id))
+			if (!this.data.channels[conf.id]) {
+				this.send(`Je ne trouve pas le channel \`${conf.name}\`, du coup RIP :'(`, message.channel)
+				return false
+			}
+		}
+		return true
+	}
+
+	commandInit(message) {
+		this.reset()
+		this.data.guild = message.guild
+		if (this.initChannels()) {
+			this.data.status = 'init'
+			this.send(`Allez hop hop hop on fait un petit ${this.generateCommand("register (pseudo)")}, on se dépêche !`)
+			this.registerMember(message.author, this.data.guild.members.get(message.author.id))
+		}
+	}
+
+	commandRegister(message) {
+		if (this.data.start) {
+			this.send(`Déso <@${message.author.id}> mais c'est déjà lancé, t'avais qu'à être à l'heure :3`)
+		} else {
+			this.registerMember(message.author, this.data.guild.members.get(message.author.id))
+		}
+	}
+
+	commandStart(message) {
+		if (this.data.members.length < 3) {
+			this.send("Y'a même pas 3 personnes, ça va être triste, même pas je lance :cry:")
+			this.reset()
+		} else {
+			this.data.game.turn = 0
+			this.data.status = 'start'
+			console.log("commandStart")
+			// this.start()
+			// this.launchStartTurn()
+		}
+	}
+
+	commandVote(message) {
+	}
+
+	commandStop(message) {
+		this.send("C'est stoppé ! De toute façon vous êtes tous nuls alors bon")
+		this.reset()
+	}
+
+	commandList(message) {
+	}
+
+	commandMarry(message) {
+	}
+
+
+// TURNS
+
+	werewolfTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+	cupidonTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+	villagerTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+	hunterTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+	clairvoyantTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+	witchTurn(startup = false) {
+		if (startup) {
+		} else {
+		}
+	}
+
+
+// START
+
+	launchStartTurn() {
+	}
+
+	start() {
+	}
+
+	setRoles() {
+		var _this = this
+		var conf = []
+		for (var i in c_roles) {
+			var role = {id: c_roles[i].id, mandatory: c_roles[i].mandatory ? true : false, optional: !c_roles[i].mandatory}
+			if (c_roles[i].fixed_number) {
+				role.ratio = c_roles[i].fixed_number
+			} else {
+				role.ratio = Math.floor(c_roles[i].ratio * this.data.members.length)
+				if (role.ratio <= 0) {
+					role.ratio = 1
+				}
+			}
+			if (!role.mandatory && Utils.rand() % 3 == 0) {
+				role.optional = true
+			}
+			if (role.mandatory || Utils.rand() % 4) {
+				conf.push(role)
+			}
+		}
+		conf.sort(function(a, b) {
+			if ((a.ratio > b.ratio && !b.mandatory) || a.mandatory) {
+				return -1;
+			} else if (a.ratio == b.ratio) {
+				return a.mandatory ? -1 : (b.mandatory ? 1 : 0)
+			} else {
+				return 1
+			}
+		})
+		var shuffled = this.shuffle(this.data.members)
+		var current_role_id = 0
+		var current_role = conf[current_role_id]
+		shuffled.forEach(function(member) {
+			if (current_role.ratio > 0) {
+				member.role = current_role.id
+				current_role.ratio--;
+				if (current_role.ratio == 0) {
+					current_role_id++;
+					current_role = current_role_id == conf.length ? 'villager' : conf[current_role_id]
+				}
+			}
+		})
+		this.data.members.forEach(function(member) {
+			member.role = shuffled.find((i) => member.id == i.id).role
+		})
+	}
+
+
+// STOP
+
+
+	reset() {
+		this.initData()
+	}
+
+
+// REGISTER
+
+
+	registerMember(member, guild_member) {
+		if (this.data.members.find((i) => (i.id == member.id))) {
+			this.send(`<@${member.id}> tu croyais m'avoir, on se register pas 2 fois comme ça ici :rage:`)
+		} else {
+			var nickname = guild_member.nickname ? guild_member.nickname : member.username
+			this.data.members.push({id: member.id, nickname: nickname})
+			this.send(`**${nickname}** a rejoint la partie, ${["on s'enjaille", "on s'amuse", "vous pouvez quitter du coup"][Utils.rand(2)]}`)
+		}
+	}
+
+
+// MESSAGING
+
+
+	send(txt, channel = null) {
+		(channel ? channel : this.data.channels.lobby).send(txt)
+	}
+
+	sendDM(txt, id) {
+		this.data.guild.members.get(id).send(txt)
+	}
+
+
+// INIT AND MISC
+
+
+	initData() {
+		this.data = {
+			status: '',
+			members: [],
+			commands: [
+				{ id: 'init', method: "commandInit", needs_init: false, needs_start: false, authorized_channels: ['lobby', 'history'] },
+				{ id: 'register', method: "commandRegister", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
+				{ id: 'start', method: "commandStart", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
+				{ id: 'vote', method: "commandVote", needs_init: true, needs_start: true, authorized_channels: ['lobby'] },
+				{ id: 'stop', method: "commandStop", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
+				{ id: 'list', method: "commandList", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
+				{ id: 'marry', method: "commandMarry", needs_init: true, needs_start: true, authorized_channels: [] },
+				{ id: 'test', method: "commandTest", needs_init: false, needs_start: false, authorized_channels: ['lobby'] },
+			],
+			command: {
+				args: null,
+				name: null,
+			},
+			game: {
+				current_role: '',
+				turn: 0,
+			},
+			channels: {
+				config: [
+					{ id: 'lobby', name: 'lobby', discord_id: '586174272687308830' },
+					{ id: 'history', name: 'historique', discord_id: '586169619564199956' },
+					{ id: 'general', name: 'general', discord_id: '586168794087686145' },
+				]
+			}
+		}
+	}
+
+	parseCommand(txt, fill_data = true) {
+		var cmd = txt.split(" ")
+		cmd.splice(0, 1)
+		if (fill_data) {
+			this.data.command.name = cmd[0]
+			this.data.command.args = cmd.slice(1)
+		}
+		return cmd
+	}
+
+	generateCommand(cmd) {
+		return `\`${this.constants.command_prefix + this.config.command_name} ${cmd}\``
+	}
+
+	shuffle(array) {
+    let counter = array.length;
+    while (counter > 0) {
+        let index = Utils.rand(counter - 1);
+        counter--;
+        let temp = array[counter];
+        array[counter] = array[index];
+        array[index] = temp;
+    }
+
+    return array;
+	}
+
+	getRole(role) {
+		return c_roles.find((i) => (i.id == role))
+	}
+
+	getMember(key, value) {
+		return this.data.members.find((i) => i[key] == value)
+	}
+
+	getMembers(role) {
+		var members = []
+		if (role) {
+			this.data.members.forEach(function(member) {
+				if (member.role == role) {
+					members.push(member)
+				}
+			})
+		} else {
+			members = this.data.members
+		}
+		return members
+	}
+
+	getMembersList(array, show_role) {
+		var txt = ""
+		var _this = this
+		array.forEach(function(member) {
+			txt += ` - **${member.nickname}**${member.role == show_role || show_role == 'all' ? `  -- **${_this.getRole(member.role).name}**` : ``}\n`
+		})
+		return txt
+	}
+}
+
+module.exports = new LesTerroristesDeThiercelieux({
+	triggered_at: "message",
+	triggered_when_command: true
+})
+
+
+/*
+const BaseModule = require("./BaseModule.js")
+const Utils = require("../Utils.js")
+
+const Discord = require("discord.js")
+
+const c_roles = [
+	{
+		id: 'villager',
+		name: "Péon",
+		ratio: 1,
+	},
+	{
+		id: 'werewolf',
+		name: "Terroriste",
+		ratio: 0.4,
+		mandatory: true
+	},
+	{
+		id: 'hunter',
+		name: "Cancer",
+		fixed_number: 1,
+		ratio: 0.1,
+	},
+	{
+		id: 'clairvoyant',
+		name: "Témoin de Jéhovah",
+		fixed_number: 1,
+		ratio: 0.1,
+	},
+	{
+		id: 'cupidon',
+		name: "Bébé avec un arc qui vole",
+		ratio: 0.1,
+		fixed_number: 1,
+	},
+	{
+		id: 'witch',
+		name: "Chimiothérapeute",
+		fixed_number: 1,
+		ratio: 0.1
+	}
+]
+
+const c_play_order_start = ["cupidon"]
+const c_play_order = ["clairvoyant", "werewolf", "witch"]
+
+class LesTerroristesDeThiercelieux extends BaseModule {
+	constructor(conf) {
+		conf.command_name = "graou";
 		conf.channel_name = "les-terroristes-de-thiercelieux"
 		super(conf)
 		this.initData()
@@ -64,7 +454,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 			if (this.data.commands[this.data.command.name]) {
 				this[this.data.commands[this.data.command.name]](message)
 			} else {
-				this.send(`${this.generateCommand(this.data.command.name)} ça existe pas :rage:`, this.data.init ? this.data.channel : message.channel)
+				this.send(`${this.generateCommand(this.data.command.name)} ça existe pas :rage:`, this.data.init ? this.data.channels : message.channel)
 			}
 		}
 	}
@@ -105,7 +495,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 	commandInit(message) {
 		// this.data.init = true
-		// this.data.started = true
+		// this.data.start = true
 		// this.data.members.push({id: 0, nickname: "a", original_nickname: null, role: 'werewolf'})
 		// this.data.members.push({id: 1, nickname: "b", original_nickname: null, role: 'villager'})
 		// this.data.members.push({id: 2, nickname: "c", original_nickname: null, role: 'werewolf'})
@@ -114,10 +504,10 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		// this.setRoles()
 		// return
 
-		this.data.channel = message.guild.channels.find('name', this.config.channel_name)
+		this.data.channels = message.guild.channels.find('name', this.config.channel_name)
 		this.data.guild = message.guild
-		if (this.data.channel.id != message.channel.id) {
-			this.send(`Faut le faire dans <#${this.data.channel.id}>, gardons ce serveur calme et serein :3`, message.channel)
+		if (this.data.channels.id != message.channel.id) {
+			this.send(`Faut le faire dans <#${this.data.channels.id}>, gardons ce serveur calme et serein :3`, message.channel)
 		} else {
 			this.data.init = true
 			this.send(`Allez hop hop hop on fait un petit ${this.generateCommand("register (pseudo)")}, on se dépêche !`)
@@ -129,7 +519,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		if (!this.checkIfInitialized(message)) {
 			return ;
 		}
-		if (this.data.started) {
+		if (this.data.start) {
 			this.send(`Déso <@${message.author.id}>, c'est déjà lancé, t'avais qu'à être à l'heure :3`)
 		} else {
 			this.registerMember(message.author, message.guild.members.get(message.author.id), this.parseCommand(message.content, false).slice(1))
@@ -145,7 +535,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 			this.reset()
 		} else {
 			this.data.turn = 0
-			this.data.started = true
+			this.data.start = true
 			this.start()
 			this.launchStartTurn()
 		}
@@ -161,7 +551,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		// 	if (this.getMember('id', message.author.id).role != 'werewolf') {
 
 		// 	}
-		// 	// if (!this.data.init || !this.data.started) {
+		// 	// if (!this.data.init || !this.data.start) {
 		// 	// 	this.send("Mshllh du calme, ça n'a même pas encore commencé", message.channel)
 		// 	// }
 		// 	var name = this.parseCommand(message.content, false)[1]
@@ -194,7 +584,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		var txt = "Joueurs présents: \n"
 		this.data.members.forEach(function(member) {
 			txt += `- \`${member.nickname}\``
-			if (_this.data.started && member.role == "werewolf" && see_werewolves) {
+			if (_this.data.start && member.role == "werewolf" && see_werewolves) {
 				txt += `  -- **${_this.getRole(member.role).name}**`
 			}
 			txt += "\n"
@@ -232,10 +622,6 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		this.sendDM(`Eh bien tu es marié, félicitations ! _mskn_\n\nTon charmant partenaire est: **${m1.nickname}**`, m2.id)
 		this.sendDM(`Eh bien tu es marié, félicitations ! _mskn_\n\nTon charmant partenaire est: **${m2.nickname}**`, m1.id)
 	}
-
-/*
- * GAME
- */
 
 
 // TURNS
@@ -381,7 +767,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		}
 		this.data.members = []
 		this.data.init = false
-		this.data.started = false
+		this.data.start = false
 	}
 
 
@@ -415,7 +801,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 
 	send(txt, channel = null) {
-		(channel ? channel : this.data.channel).send(txt)
+		(channel ? channel : this.data.channels).send(txt)
 	}
 
 	sendDM(txt, id) {
@@ -436,7 +822,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 	initData() {
 		this.data = {
-			started: false,
+			start: false,
 			init: false,
 			members: [],
 			commands: {
@@ -522,3 +908,5 @@ module.exports = new LesTerroristesDeThiercelieux({
 	triggered_at: "message",
 	triggered_when_command: true
 })
+*/
+
