c78fee2b695cd40b7e14bd9f4661976f44d404a5
Sylvain Chaugny <sylvain@slaask.com>
2019-06-12 18:03:46 +0200

    [ADD]

--- modules/LesTerroristesDeThiercelieux.js
+++ modules/LesTerroristesDeThiercelieux.js
@@ -44,9 +44,225 @@ const c_roles = [
 const c_play_order_start = ["cupidon"]
 const c_play_order = ["clairvoyant", "werewolf", "witch"]
 
+
+
+
+
+
+
+class Log {
+	static send(txt, channel, timeout = 0) {
+		if (timeout) {
+			setTimeout(() => channel.send(txt), timeout)
+		} else {
+			channel.send(txt)
+		}
+	}
+}
+
+
+
+
+
+
+
+
+class MemberFactory {
+	constructor() {
+		this.members = []
+	}
+
+	create(data) {
+		var member = new Member()
+		member.init(data)
+		this.members.push(member)
+		return member
+	}
+
+	findUnique(fn) {
+		return this.members.find(fn)
+	}
+
+	find(fn) {
+		var res = []
+		for (member of this.members) {
+			if (fn(member)) {
+				res.push(member)
+			}
+		}
+		return res
+	}
+}
+
+class Member {
+	constructor() {
+		this.data = {
+			nickname: '',
+			id: ''
+			roles: [],
+			discord: {
+				guild_member: null,
+				id: null,
+			}
+		}
+	}
+
+	init(data) {
+		this.data = data
+	}
+
+	send(txt, timeout = 0) {
+		Log.send(txt, this.discord.guild_member, timeout)
+	}
+
+	setRole(role) {
+	}
+}
+
+
+
+
+
+
+class ChannelFactory {
+	constructor() {
+		this.channels = []
+	}
+
+	create(data) {
+		var channel = new Channel()
+		channel.init(data)
+		this.channels.push(channel)
+		return channel
+	}
+}
+
+class Channel {
+	constructor() {
+		this.data = {
+			id: '',
+			role: '',
+			discord: {
+				channel: null,
+				id: null
+			}
+		}
+	}
+
+	init(data) {
+		this.data = data
+	}
+
+	send(txt, timeout = 0) {
+		Log.send(txt, this.discord.channel, timeout)
+	}
+
+	assign(member) {
+		this.discord.channel.overwritePermissions(member.discord.member, {
+			SEND_MESSAGES: false,
+			VIEW_CHANNEL: true,
+		})
+	}
+
+	setSendMessagesState(state) {
+		for (var member of _members.find((m) => m.hasRole('role'))) {
+			this.discord.channel.overwritePermissions(member.discord.member, {
+				SEND_MESSAGES: state,
+			})
+		}
+	}
+
+	disable() {
+
+	}
+
+	enable() {
+
+	}
+
+	flush() {
+
+	}
+}
+
+
+
+
+
+class RoleFactory {
+	constructor() {
+		this.roles = []
+	}
+
+	create(data) {
+		var role = new Role()
+		role.init(data)
+		this.roles.push(role)
+		return role
+	}
+}
+
+class Role {
+	constructor() {
+		this.data = {
+			id: '',
+			name: '',
+		}
+	}
+
+	init(data) {
+		this.data = data
+	}
+}
+
+
+
+
+
+class CommandFactory {
+	constructor() {
+		this.commands = []
+	}
+
+	create(data) {
+		var command = new Command()
+		command.init(data)
+		this.commands.push(command)
+		return command
+	}
+}
+
+class Command {
+	constructor() {
+		this.data = {
+			authorized_channels: [],
+			required_status: ''
+		}
+	}
+
+	init(data) {
+		this.data = data
+	}
+
+	exec(message) {
+
+	}
+}
+
+
+
+var _members = new MemberFactory()
+var _roles = new RoleFactory()
+var _channels = new ChannelFactory()
+var _commands = new CommandFactory()
+
+
+
+
 class LesTerroristesDeThiercelieux extends BaseModule {
 	constructor(conf) {
 		conf.command_name = "graou";
+		conf.admin_role = '586169000623341588';
 		super(conf)
 		this.initData()
 	}
@@ -56,6 +272,10 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 	process(message) {
+		if (message.channel.type == "dm") {
+			message.channel.send("Pas de DM :rage:")
+			return
+		}
 		this.parseCommand(message.content)
 		if (this.data.waiting_command && this.data.command.name != this.data.waiting_command) {
 			this.send(`Calmons-nous <@${message.author.id}>, j'attends quelqu'un et après on verra pour toi :3`, this.data.status.length ? null : message.channel)
@@ -69,6 +289,12 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 				this.send(`${this.generateCommand(this.data.command.name)} ça existe pas :rage:`, this.data.status.length ? null : message.channel)
 			}
 		}
+		this.flushCommand()
+	}
+
+	flushCommand() {
+		this.data.command.name = null
+		this.data.command.args = null
 	}
 
 	canTriggerCommand(message, command) {
@@ -89,7 +315,9 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 				}
 				return true
 			}
-			if (this.data.status != 'init') {
+			if (this.data.status == 'start' || this.data.status == 'init') {
+				return true
+			} else {
 				this.send(`Calmons-nous <@${message.author.id}>, faut d'abord faire un ${this.generateCommand("init")} pour ça !`, message.channel)
 				return false
 			}
@@ -99,23 +327,43 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 	commandTest(message) {
+		if (this.i != null) {
+			this.i++
+		} else {
+			this.i = 0
+		}
+		if (this.i % 2 == 0) {
+			this.enableChannel('villager')
+//			message.guild.members.find((i) => (i.id == message.author.id)).addRole(this.config.admin_role)
+		} else {
+			this.disableChannel('villager')
+//			message.guild.members.find((i) => (i.id == message.author.id)).removeRole(this.config.admin_role)
+		}
 	}
 
-	initChannels() {
+	initChannels(message) {
 		for (var conf of this.data.channels.config) {
-			this.data.channels[conf.id] = this.data.guild.channels.find((found) => (found.id == conf.discord_id))
-			if (!this.data.channels[conf.id]) {
+			var var_name = conf.id.replace('-', '_')
+			this.data.channels[var_name] = this.getChannel('id', conf.discord_id)
+			if (!this.data.channels[var_name]) {
 				this.send(`Je ne trouve pas le channel \`${conf.name}\`, du coup RIP :'(`, message.channel)
 				return false
 			}
+			this.flushChannel(this.data.channels[var_name], conf)
 		}
 		return true
+	}
+
+	flushChannel(channel, config) {
+		if (config.role_channel) {
+			channel.fetchMessages().then(messages => channel.bulkDelete(messages));
+		}
 	}
 
 	commandInit(message) {
 		this.reset()
 		this.data.guild = message.guild
-		if (this.initChannels()) {
+		if (this.initChannels(message)) {
 			this.data.status = 'init'
 			this.send(`Allez hop hop hop on fait un petit ${this.generateCommand("register (pseudo)")}, on se dépêche !`)
 			this.registerMember(message.author, this.data.guild.members.get(message.author.id))
@@ -131,15 +379,20 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 	commandStart(message) {
-		if (this.data.members.length < 3) {
-			this.send("Y'a même pas 3 personnes, ça va être triste, même pas je lance :cry:")
+		if (this.data.members.length < this.data.min_players) {
+			this.send(`Y'a même pas ${this.data.min_players} personnes, ça va être triste, même pas je lance :cry:`)
 			this.reset()
 		} else {
 			this.data.game.turn = 0
 			this.data.status = 'start'
-			console.log("commandStart")
-			// this.start()
-			// this.launchStartTurn()
+			this.start()
+			this.nextTurn()
+		}
+	}
+
+	nextTurn() {
+		if (this.data.game.turn == 0) {
+			this.launchStartTurn()
 		}
 	}
 
@@ -160,49 +413,107 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 // TURNS
 
-	werewolfTurn(startup = false) {
-		if (startup) {
+	werewolfTurn(step = 'turn') {
+		var wolves = this.getMembers('role', 'werewolf')
+		if (step == 'welcome') {
+			this.send(`Coucou ! Bienvenue sur le channel réversé aux combattants de la liberté <3`, this.data.channels.werewolf_channel, 750)
+		} else if (step == 'start') {
 		} else {
 		}
 	}
 
-	cupidonTurn(startup = false) {
-		if (startup) {
+	cupidonTurn(step = 'turn') {
+		var cupidon = this.getMember('role', 'cupidon')
+		if (!cupidon) {
+			return
+		}
+		if (step == 'welcome') {
+			this.send(`Coucou bébé qui pleure ! Tu vas pouvoir choisir 2 personnes qui vont s'aimer pour toujours :3`, this.data.channels.cupidon_channel, 750)
+			this.data.waiting_command = "marry"
+		} else if (step == 'start') {
+			this.send(`Au tour du bébé qui vole, qui va unir pour leur plus grand bonheur 2 personnes parmi vous...`)
+		}
+	}
+
+	villagerTurn(step = 'turn') {
+		if (step == 'welcome') {
+			this.send(`Vous êtes ici dans le fameux village de Hénin-Beaumont, ~~fief du FN~~ heureuse bourgade paisible et prospère !\nAucune attaque de terroristes en vue, vous pouvez dormir sur vos 2 oreilles !`, this.data.channels.villager_channel, 750)
+		} else if (step == 'start') {
 		} else {
 		}
 	}
 
-	villagerTurn(startup = false) {
-		if (startup) {
+	hunterTurn(step = 'turn') {
+		if (step == 'welcome') {
+		} else if (step == 'start') {
 		} else {
 		}
 	}
 
-	hunterTurn(startup = false) {
-		if (startup) {
+	clairvoyantTurn(step = 'turn') {
+		if (step == 'welcome') {
+		} else if (step == 'start') {
 		} else {
 		}
 	}
 
-	clairvoyantTurn(startup = false) {
-		if (startup) {
+	witchTurn(step = 'turn') {
+		if (step == 'welcome') {
+		} else if (step == 'start') {
 		} else {
 		}
 	}
 
-	witchTurn(startup = false) {
-		if (startup) {
-		} else {
+	setChannelSendMessagesState(role, state) {
+		var channel = this.getChannel('id', this.getChannelConfig('id', role + "-channel").discord_id)
+		for (var member of this.getMembers('role', role)) {
+			channel.overwritePermissions(this.getGuildMember('id', member.id), {
+				SEND_MESSAGES: state,
+			})
 		}
 	}
 
+	disableChannel(role) {
+		this.setChannelSendMessagesState(role, true)
+	}
+
+	enableChannel(role) {
+		this.setChannelSendMessagesState(role, false)
+	}
+
+	assignMemberToChannel(member, channel_id) {
+		var channel = this.getChannel('id', this.getChannelConfig('id', channel_id).discord_id)
+		channel.overwritePermissions(this.getGuildMember('id', member.id), {
+			SEND_MESSAGES: false,
+			VIEW_CHANNEL: true,
+		})
+	}
 
 // START
 
 	launchStartTurn() {
+		for (var member of this.data.members) {
+			this.assignMemberToChannel(member, member.role + "-channel")
+			if (member.role != "villager") {
+				this.assignMemberToChannel(member, "villager-channel")
+			}
+		}
+		for (var role of c_roles) {
+			this[role.id + "Turn"]('welcome')
+		}
+		for (var role of c_play_order_start) {
+			this[role + "Turn"]('start')
+		}
 	}
 
 	start() {
+		var notifs = ""
+		this.setRoles()
+		for (var member of this.data.members) {
+			notifs += "<@" + member.id + "> "
+			this.getGuildMember('id', member.id).removeRole(this.config.admin_role)
+		}
+		this.send(`${notifs}Allez on est tipar ! J'attribue les rôles maintenant, checkez vos DM :3`)
 	}
 
 	setRoles() {
@@ -249,6 +560,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 		})
 		this.data.members.forEach(function(member) {
 			member.role = shuffled.find((i) => member.id == i.id).role
+			member.role = 'werewolf'
 		})
 	}
 
@@ -257,6 +569,17 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 
 	reset() {
+		for (var member of this.data.members) {
+			this.getGuildMember('id', member.id).addRole(this.config.admin_role)
+			for (var channel of this.data.channels.config) {
+				if (channel.role_channel) {
+					this.getChannel('id', channel.discord_id).overwritePermissions(this.getGuildMember('id', member.id), {
+						SEND_MESSAGES: !channel.role_channel,
+						VIEW_CHANNEL: !channel.role_channel,
+					})
+				}
+			}
+		}
 		this.initData()
 	}
 
@@ -278,14 +601,18 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 // MESSAGING
 
 
-	send(txt, channel = null) {
-		(channel ? channel : this.data.channels.lobby).send(txt)
+	send(txt, channel = null, timeout = 0) {
+		if (timeout) {
+			var _this = this
+			setTimeout(() => ((channel ? channel : _this.data.channels.lobby).send(txt)), timeout)
+		} else {
+			(channel ? channel : this.data.channels.lobby).send(txt)
+		}
 	}
 
 	sendDM(txt, id) {
 		this.data.guild.members.get(id).send(txt)
 	}
-
 
 // INIT AND MISC
 
@@ -295,14 +622,14 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 			status: '',
 			members: [],
 			commands: [
-				{ id: 'init', method: "commandInit", needs_init: false, needs_start: false, authorized_channels: ['lobby', 'history'] },
+				{ id: 'init', method: "commandInit", needs_init: false, needs_start: false, authorized_channels: ['lobby'] },
 				{ id: 'register', method: "commandRegister", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
 				{ id: 'start', method: "commandStart", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
 				{ id: 'vote', method: "commandVote", needs_init: true, needs_start: true, authorized_channels: ['lobby'] },
 				{ id: 'stop', method: "commandStop", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
 				{ id: 'list', method: "commandList", needs_init: true, needs_start: false, authorized_channels: ['lobby'] },
 				{ id: 'marry', method: "commandMarry", needs_init: true, needs_start: true, authorized_channels: [] },
-				{ id: 'test', method: "commandTest", needs_init: false, needs_start: false, authorized_channels: ['lobby'] },
+				{ id: 'test', method: "commandTest", needs_init: false, needs_start: false, authorized_channels: ['lobby', 'history', 'general'] },
 			],
 			command: {
 				args: null,
@@ -317,13 +644,26 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 					{ id: 'lobby', name: 'lobby', discord_id: '586174272687308830' },
 					{ id: 'history', name: 'historique', discord_id: '586169619564199956' },
 					{ id: 'general', name: 'general', discord_id: '586168794087686145' },
+					{ id: 'villager-channel', name: 'place-du-village', discord_id: '586169842135072776', role_channel: true },
+					{ id: 'werewolf-channel', name: 'bled', discord_id: '586171506673844234', role_channel: true },
+					{ id: 'witch-channel', name: 'service-de-chimio', discord_id: '586170490180337675', role_channel: true },
+					{ id: 'cupidon-channel', name: 'love-motel', discord_id: '586176105116073994', role_channel: true },
+					{ id: 'clairvoyant-channel', name: 'eglise-de-jehovah', discord_id: '586170550095708171', role_channel: true },
+					{ id: 'hunter-channel', name: 'sois-paliatifs', discord_id: '586170721341014056', role_channel: true },
+					{ id: 'dead-channel', name: 'cimetière', discord_id: '586169866294394881', role_channel: true },
 				]
-			}
+			},
+			min_players: 1
 		}
 	}
 
 
-
+	getChannelConfig(key, value) {
+		return this.data.channels.config.find((chan) => (chan[key] == value))
+	}
+	getChannel(key, value) {
+		return this.data.guild.channels.find((chan) => (chan[key] == value))
+	}
 
 	parseCommand(txt, fill_data = true) {
 		var cmd = txt.split(" ")
@@ -357,19 +697,19 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 	getMember(key, value) {
-		return this.data.members.find((i) => i[key] == value)
+		return this.data.members.find((i) => (i[key] == value))
 	}
 
-	getMembers(role) {
+	getGuildMember(key, value) {
+		return this.data.guild.members.find((i) => (i[key] == value))
+	}
+
+	getMembers(key, value) {
 		var members = []
-		if (role) {
-			for (member of this.data.members) {
-				if (member.role == role) {
-					members.push(member)
-				}
+		for (var member of this.data.members) {
+			if (member[key] == value) {
+				members.push(member)
 			}
-		} else {
-			members = this.data.members
 		}
 		return members
 	}
@@ -629,7 +969,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 // TURNS
 
 	werewolfTurn(startup = false) {
-		if (startup) {
+		if (step == 'start') {
 			var list = this.getMembersList(this.getMembers('werewolf'))
 			var _this = this
 			this.getMembers('werewolf').forEach(function(member) {
@@ -641,7 +981,7 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 
 	cupidonTurn(startup = false) {
 		var cupidon = this.getMember('role', 'cupidon')
-		if (startup) {
+		if (step == 'start') {
 			this.data.waiting_command = "marry"
 			this.sendDM(`Voici les joueurs:\n${this.getMembersList(this.data.members)}\n\nFais un ${this.generateCommand("marry pseudo_1 pseudo_2")} pour lier 2 personnes entre elles _(c'est rigolo, on s'amuse)_`, cupidon.id)
 		} else {
@@ -649,25 +989,25 @@ class LesTerroristesDeThiercelieux extends BaseModule {
 	}
 
 	villagerTurn(startup = false) {
-		if (startup) {
+		if (step == 'start') {
 		} else {
 		}
 	}
 
 	hunterTurn(startup = false) {
-		if (startup) {
+		if (step == 'start') {
 		} else {
 		}
 	}
 
 	clairvoyantTurn(startup = false) {
-		if (startup) {
+		if (step == 'start') {
 		} else {
 		}
 	}
 
 	witchTurn(startup = false) {
-		if (startup) {
+		if (step == 'start') {
 		} else {
 		}
 	}
